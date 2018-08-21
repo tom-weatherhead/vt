00cee5938d224a88a2bc6c3fef0d968ec160443d
Tom Weatherhead <tom_weatherhead@hotmail.com>
2018-08-21 13:28:15 -0600

    Express.js server: Use the npm package cors

--- server/src/app.js
+++ server/src/app.js
@@ -14,14 +14,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
-//app.use(cors());
+app.use(cors());
 
 // Cross Origin middleware (Chris Ganga)
+/*
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
+*/
 
 /*
 app.options('/*', function(req, res, next) {
